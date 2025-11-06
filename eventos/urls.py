7e3e196fbf5197e23279edc44e509f2d36b7d9ab
Giovanna Lisboa <silvalisboag@gmail.com>
2025-11-06 12:17:02 -0300

    Adicionando layout a view do cliente

--- eventos/urls.py
+++ eventos/urls.py
@@ -1,13 +1,14 @@
-# eventos/urls.py
+
 
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    # 1. Listagem (Página Inicial)
+    
     path('', views.lista_eventos, name='lista_eventos'), 
     
-    # 2. Detalhe do Evento
+    
     path('evento/<int:pk>/', views.detalhe_evento, name='detalhe_evento'), 
     
     path('comprar/<int:ingresso_pk>/', views.comprar_ingresso, name='comprar_ingresso'),
--- eventos/views.py
+++ eventos/views.py
@@ -1,20 +1,19 @@
-# eventos/views.py
+
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Evento, Ingresso, Compra, Local, Categoria
+from .models import Evento 
 from .forms import EventoForm, IngressoFormSet
-from django.db import transaction
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-# 1. View de Listagem
+
 def lista_eventos(request):
     eventos = Evento.objects.all().order_by('data_evento')
     context = {
         'eventos': eventos
     }
-    # ATENÇÃO: O caminho DEVE ser 'eventos/lista_eventos.html'
+   
     return render(request, 'eventos/lista_eventos.html', context)
 
-# 2. View de Detalhe
+
 def detalhe_evento(request, pk):
     evento = get_object_or_404(Evento, pk=pk) 
     context = {
